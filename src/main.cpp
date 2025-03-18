1ad2545a29f94395f70869fdfc9dab8451938247
AliAngel564 <jonathan.aliangel@gmail.com>
2025-03-18 15:56:48 -0600

    finished program

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,20 @@
-
+/*
+Amerike University
+Author: Ali Angel
+Work #: 15
+Date: 18/03/25
+Description: This program will use arrays and functions to ask the user for 10 words and an additional word
+and it will then look for the additional word in the array, it will then show the amount of times the word
+was repeated and the index number of the array it appeared in.
+*/
 
 #include <iostream>
 #include <conio.h>
-#include <string>
 
 using namespace std;
 
-void getWords(string array[], int size);
 void pressAnyKey();
+void getWords(string array[], int size);
 void lookForWord(string array[],int size,string extraWord);
 
 int main()
@@ -44,13 +51,15 @@ void lookForWord(string array[],int size,string extraWord)
 {
     int index[10];
     int countRepeated = 0;
-    for (int i = 0; i < size; i++)
+    int counter = 0;
+    int indexNum;
+    for (indexNum = 0; indexNum < size; indexNum++)
     {
-        if(extraWord == array[i])
+        if(extraWord == array[indexNum])
         {
-            int indexNum = i;
-            index[i] = indexNum;
             countRepeated++;
+            index[counter] = indexNum;
+            counter++;
         }
     }
     if(countRepeated == 0)
@@ -59,7 +68,7 @@ void lookForWord(string array[],int size,string extraWord)
     }else
     {
         cout << "\nOur additional word was found " << countRepeated << " times in our array";
-         cout << "\nThe word was found in the following indexes of our array\n";
+        cout << "\nThe word was found in the following indexes of our array\n";
         for(int i=0;i<countRepeated;i++)
         {
             cout << "index No." << index[i] << "\n";
